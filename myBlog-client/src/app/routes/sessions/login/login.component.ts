c20c4a5a66342c221e7922e8533b55ff1cb9ec8a
zl <786253795@qq.com>
2020-12-18 12:44:51 +0800

    login and register

--- myBlog-client/src/app/routes/sessions/login/login.component.ts
+++ myBlog-client/src/app/routes/sessions/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { SettingsService, StartupService, TokenService } from '@core';
+import { HttpClient } from "@angular/common/http";  //这里是HttpClient
 
 @Component({
   selector: 'app-login',
@@ -16,7 +17,9 @@ export class LoginComponent implements OnInit {
     private router: Router,
     private token: TokenService,
     private startup: StartupService,
-    private settings: SettingsService
+    private settings: SettingsService,
+    private $http: HttpClient,
+    // public resList: any,
   ) {
     this.loginForm = this.fb.group({
       // username: ['', [Validators.required, Validators.pattern('ng-matero')]],
@@ -37,28 +40,50 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
-    
-    const { token, uid, username } = { 
-      token: 'ng-matero-token', 
-      uid: 1, 
-      username: 'zl',
+    var name = this.loginForm.get('username').value;
+    var pass = this.loginForm.get('password').value;
+    var formData = {
+      username : name,
+      password : pass,
     };
-    // Set user info
-    this.settings.setUser({
-      id: uid,
-      name: 'Zongbin',
-      email: 'nzb329@163.com',
-      avatar: './assets/images/avatar.jpg',
-    });
-    // Set token info
-    this.token.set({ token, uid, username });
-    // Regain the initial data
-    this.startup.load().then(() => {
-      let url = this.token.referrer!.url || '/';
-      if (url.includes('/auth')) {
-        url = '/';
-      }
-      this.router.navigateByUrl(url);
-    });
+    // console.log(JSON.stringify(formData));
+    // 发送登录请求
+    this.$http.post("http://172.26.43.243:9999/user/login",formData).subscribe(res=>{ 
+      //this.resList = res;
+      var res_string = JSON.stringify(res);
+      var res_data = {
+        Message:"",
+      };
+      res_data = JSON.parse(res_string);
+      // console.log(res_data.Message);
+      var id_string = JSON.stringify(res_data.Message);
+      var id = JSON.parse(id_string).Id;
+      console.log(id);
+      this.$http.post("http://172.26.43.243:9999/user","").subscribe(res=>{
+        // const { token, uid, username } = { 
+        //   token: 'ng-matero-token', 
+        //   uid: 1, 
+        //   username: 'zl',
+        // };
+        // Set user info
+        console.log(res);
+        // this.settings.setUser({
+        //   id: id,
+        //   name: 'Zongbin',
+        //   email: 'nzb329@163.com',
+        //   avatar: './assets/images/avatar.jpg',
+        // });
+        // // Set token info
+        // this.token.set({ token, uid, username });
+        // // Regain the initial data
+        // this.startup.load().then(() => {
+        //   let url = this.token.referrer!.url || '/';
+        //   if (url.includes('/auth')) {
+        //     url = '/';
+        //   }
+        //   this.router.navigateByUrl(url);
+        // });
+      })
+     });
   }
 }
--- myBlog-client/src/app/routes/sessions/register/register.component.ts
+++ myBlog-client/src/app/routes/sessions/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
+import { HttpClient } from "@angular/common/http";  //这里是HttpClient
 
 @Component({
   selector: 'app-register',
@@ -8,7 +10,11 @@ import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms'
 export class RegisterComponent implements OnInit {
   registerForm: FormGroup;
 
-  constructor(private fb: FormBuilder) {
+  constructor(
+    private fb: FormBuilder,
+    private router: Router,
+    private $http: HttpClient,
+  ) {
     this.registerForm = this.fb.group({
       username: ['', [Validators.required]],
       password: ['', [Validators.required]],
@@ -17,6 +23,30 @@ export class RegisterComponent implements OnInit {
   }
 
   ngOnInit() {}
+
+  register(){
+    console.log("submit to register.");
+    var name = this.registerForm.get('username').value;
+    var pass = this.registerForm.get('password').value;
+    var formData = {
+      username : name,
+      password : pass,
+    };
+    // console.log(JSON.stringify(formData));
+    // 发送登录请求
+    this.$http.post("http://172.26.43.243:9999/user/register",formData).subscribe(res=>{ 
+      console.log(JSON.stringify(res)); 
+      var res_string = JSON.stringify(res);
+      var res_data = {
+        Type:"fail",
+      };
+      res_data = JSON.parse(res_string);
+      if(res_data.Type == "success"){
+        console.log("here");
+        this.router.navigateByUrl("/login");
+      }
+     });
+  }
 
   confirmValidator = (control: FormControl): { [k: string]: boolean } => {
     if (!control.value) {
